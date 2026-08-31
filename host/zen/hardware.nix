31fe209481e6761f50df253fd645118586a126c1
aetheris <aetheris.cjx@gmail.com>
2026-08-31 01:52:03 -0700

    add: zen's hardware

--- host/zen/hardware.nix
+++ host/zen/hardware.nix
@@ -1,4 +1,19 @@
-{ ... }:
+{ 
+    config,
+    lib,
+    modulesPath,
+    ...
+}:
 {
+    imports = [
+        (modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
+    boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ "kvm-amd" ];
+    boot.extraModulePackages = [ ];
+
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
