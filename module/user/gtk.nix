e863fe60a3776bc166ae4f56e7f37f8783e35179
aetheris <aetheris.cjx@gmail.com>
2026-08-31 13:43:22 -0700

    fix: vendored gtk3 theme template

--- module/user/gtk.nix
+++ module/user/gtk.nix
@@ -12,7 +12,11 @@ let
     colors = metadata."theme.colors";
     mode = metadata."theme.mode";
     preferDark = if mode == "dark" then "true" else "false";
-    settings = pkgs.writeText "gtk-settings.ini" ''
+    gtk3Settings = pkgs.writeText "gtk3-settings.ini" ''
+        [Settings]
+        gtk-application-prefer-dark-theme=${preferDark}
+    '';
+    gtk4Settings = pkgs.writeText "gtk4-settings.ini" ''
         [Settings]
         gtk-application-prefer-dark-theme=${preferDark}
     '';
@@ -22,14 +26,29 @@ let
     gtk4Theme = colors {
         template = ../../template/gtk4.mustache;
     };
+    reloadTheme = pkgs.writeShellScript "reload-gtk-theme" ''
+        ${pkgs.systemd}/bin/systemctl \
+            --user \
+            try-restart xdg-desktop-portal-gtk.service
+    '';
 in
 {
     systemd.tmpfiles.rules = [
         "d ${userHome}/.config/gtk-3.0 0755 ${userName} users -"
         "L+ ${userHome}/.config/gtk-3.0/gtk.css - - - - ${gtk3Theme}"
-        "L+ ${userHome}/.config/gtk-3.0/settings.ini - - - - ${settings}"
+        "L+ ${userHome}/.config/gtk-3.0/settings.ini - - - - ${gtk3Settings}"
         "d ${userHome}/.config/gtk-4.0 0755 ${userName} users -"
         "L+ ${userHome}/.config/gtk-4.0/gtk.css - - - - ${gtk4Theme}"
-        "L+ ${userHome}/.config/gtk-4.0/settings.ini - - - - ${settings}"
+        "L+ ${userHome}/.config/gtk-4.0/settings.ini - - - - ${gtk4Settings}"
     ];
+
+    systemd.user.services."theme-reload-gtk-${userName}" = {
+        description = "Reload GTK portal after a theme change";
+        wantedBy = [ "theme-reload.target" ];
+        unitConfig.ConditionUser = userName;
+        serviceConfig = {
+            Type = "oneshot";
+            ExecStart = reloadTheme;
+        };
+    };
 }
