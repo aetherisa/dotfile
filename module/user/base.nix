5cbcac510d75bcf95aa320d78137fd435c76e006
aetheris <aetheris.cjx@gmail.com>
2026-08-31 01:53:32 -0700

    add: persist home dir

--- module/user/base.nix
+++ module/user/base.nix
@@ -2,10 +2,16 @@ metadata:
 assert builtins.hasAttr "user.name" metadata;
 assert builtins.hasAttr "user.home" metadata;
 assert builtins.hasAttr "user.modules.base" metadata;
+assert builtins.hasAttr "persistence.enable" metadata;
+assert builtins.hasAttr "persistence.userRoot" metadata;
 { ... }:
 let
     userName = metadata."user.name";
     userHome = metadata."user.home";
+    persist = {
+        enable = metadata."persistence.enable";
+        userRoot = metadata."persistence.userRoot";
+    };
 in
 {
     systemd.user.targets.theme-reload = {
@@ -16,4 +22,10 @@ in
     systemd.tmpfiles.rules = [
         "d ${userHome}/.config 0755 ${userName} users -"
     ];
+
+    environment.persistence = lib.mkIf persist.enable {
+        ${persist.userRoot}.users.${userName}.directories = [
+            "pin"
+        ];
+    };
 }
