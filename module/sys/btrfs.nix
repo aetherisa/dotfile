1c59e4edd629b73b80149e44891fc5f6ad2507db
aetheris <aetheris.cjx@gmail.com>
2026-09-01 16:33:30 -0700

    add: swapfile

--- module/sys/btrfs.nix
+++ module/sys/btrfs.nix
@@ -1,5 +1,13 @@
 metadata:
+assert builtins.hasAttr "persistence.systemRoot" metadata;
 { ... }:
 {
     services.btrfs.autoScrub.enable = true;
+
+    swapDevices = [
+        {
+            device = "${metadata."persistence.systemRoot"}/swapfile";
+            size = 16 * 1024; # 16G
+        }
+    ];
 }
