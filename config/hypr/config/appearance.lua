a8ce04188fc226d5c8ab441acfb5b6839a2e5cac
aetheris <aetheris.cjx@gmail.com>
2026-09-05 22:56:57 -0700

    change: no dim special workspace + bring border back

--- config/hypr/config/appearance.lua
+++ config/hypr/config/appearance.lua
@@ -40,6 +40,7 @@ hl.config({
 
 	decoration = {
 		rounding = 0,
+		dim_special = 0,
 
 		-- helps concentration
 		active_opacity = 0.9,
--- config/hypr/config/rules.lua
+++ config/hypr/config/rules.lua
@@ -39,15 +39,6 @@ hl.window_rule({
     opacity = "1.0 override 1.0 override 1.0 override",
 })
 
--- windows in the scratchpad should not inherit the global border
-hl.window_rule({
-	name = "borderless-special-workspace",
-	match = {
-		workspace = "special:magic",
-	},
-	border_size = 0,
-})
-
 ----------------------
 -- Workspace rules
 
